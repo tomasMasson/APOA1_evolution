ba4266d90ad3b10e7ee7c201aa403309883c6aad
tomas masson <tomasmasson0@gmail.com>
2020-06-23 03:02:50 -0300

    Plotting scripts for variants data and files cleanup

--- in-silico_mutagenesis/src/plot_rhapsody_dataset.py
+++ in-silico_mutagenesis/src/plot_rhapsody_dataset.py
@@ -1,23 +1,18 @@
 #!/usr/bin/env python3
 
-from pathlib import Path
 import argparse
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def plot_foldx_results(filename):
-    
-    mutations = ['G', 'A', 'V', 'L', 'I', 'M', 'F', 'W', 'P', 'S',
-                 'T', 'C', 'Y', 'N', 'Q', 'D', 'E', 'K', 'R', 'H']
+
+def plot_rhapsody_results(filename):
 
     df = pd.read_csv(filename, index_col='Site')
-    lenght = 243
     for i in range(3):
         start = int(243/3 * i)
         end = int(243/3 * (i+1))
-        plt.figure(figsize=(16,8))
+        plt.figure(figsize=(16, 8))
         sns.heatmap(df[start:end].T,
                     vmin=0,
                     vmax=1,
@@ -29,21 +24,24 @@ def plot_foldx_results(filename):
         name = f'rhapsody_mutagenesis{i+1}.svg'
         plt.savefig(name)
 
-def arguments_parser():    
+
+def arguments_parser():
     ''' Command line argument parser.'''
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('filename', 
+    parser.add_argument('filename',
                         type=str,
                         help='List')
     args = parser.parse_args()
 
     return args.filename
 
+
 def main():
 
     filename = arguments_parser()
-    plot_foldx_results(filename)
+    plot_rhapsody_results(filename)
+
 
 if __name__ == '__main__':
     main()
